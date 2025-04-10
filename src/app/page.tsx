449ab181f524b3e059df2916bd425137ea6a9cce
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-04-10 18:27:00 +0900

    fix(Day1)：1DayからDay1に修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className="m-10 flex h-10 w-14 text-center text-xl hover:bg-amber-700">
-        <Link href="1Day">1Day</Link>
+      <div className="m-10 flex h-10 w-14 flex-col text-center text-xl hover:bg-amber-700">
+        <Link href="Day1">Day1</Link>
+        <Link href="Day2">Day2</Link>
       </div>
     </div>
   );
