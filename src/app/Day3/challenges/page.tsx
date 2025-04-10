f12af67bc68838bc040ff7ea8f2d099e69e3c228
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-04-10 19:40:04 +0900

    feat(challenges):Day3 - challenges終了

--- src/app/Day3/challenges/page.tsx
+++ src/app/Day3/challenges/page.tsx
@@ -2,41 +2,51 @@ const Page = () => {
   return (
     <>
       {/* 1つ目のコンテンツ */}
-      <div className="mx-auto w-[80%] max-w-[750px] bg-[#23424a] px-0 py-[50px] font-sans text-white">
-        <div className="w-[50%]">
-          <h1 className="text-4xl font-bold">Lorem ipsum dolor sit.</h1>
-          <p className="my-4 font-medium">
+      <div className="bg-[#23424a]">
+        <div className="mx-auto w-[60%] py-[50px] font-sans text-white">
+          <div className="w-[50%] max-w-[400px]">
+            <h1 className="text-4xl font-bold">Lorem ipsum dolor sit.</h1>
+            <p className="my-4 font-medium">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem aspernatur distinctio laudantium dolores. Nulla
+              quibusdam reprehenderit eum sit minus aliquid!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* 2つ目のコンテンツ */}
+      <div className="mx-auto w-[60%]">
+        <div className="max-w-[750px]">
+          <h2 className="my-[1.245rem] text-3xl font-bold">more content D:</h2>
+          <p className="my-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem aspernatur distinctio laudantium dolores. Nulla
-            quibusdam reprehenderit eum sit minus aliquid!
+            Perferendis, mollitia adipisci magnam voluptatibus repellendus fuga
+            ut repellat exercitationem eaque amet, omnis aliquam fugiat
+            laudantium id dicta at? Consectetur iure porro illum laudantium
+            excepturi a laborum!
+          </p>
+          <p className="my-4">
+            Sit magni soluta porro fugit placeat eius itaque, accusamus quisquam
+            voluptates reiciendis pariatur, vitae molestiae. Minima, quos
+            reprehenderit autem animi, nisi necessitatibus eligendi quis modi,
+            facilis ipsam nihil odit quaerat! Nisi doloribus harum culpa ipsam!
+          </p>
+          <p className="my-4">
+            Sint corporis animi repudiandae. Aliquid illum, tenetur magnam
+            provident molestiae rem doloremque aspernatur quia reiciendis est
+            facilis enim praesentium officia sequi qui debitis exercitationem
+            quaerat hic quos recusandae. Architecto repudiandae aperiam tempora
+            iste saepe error.
+          </p>
+          <p className="my-4">
+            Provident aut suscipit aspernatur doloribus illum assumenda
+            cupiditate perferendis dolores! Obcaecati omnis magnam ipsam
+            voluptas ipsa eos explicabo quisquam architecto similique, sunt
+            repellendus animi a doloribus ab deserunt. Dicta neque nostrum modi,
+            illum debitis dolorem.
           </p>
         </div>
       </div>
-      <h2 className="my-[1.245rem] text-3xl font-bold">more content D:</h2>
-      <p className="my-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
-        mollitia adipisci magnam voluptatibus repellendus fuga ut repellat
-        exercitationem eaque amet, omnis aliquam fugiat laudantium id dicta at?
-        Consectetur iure porro illum laudantium excepturi a laborum!
-      </p>
-      <p className="my-4">
-        Sit magni soluta porro fugit placeat eius itaque, accusamus quisquam
-        voluptates reiciendis pariatur, vitae molestiae. Minima, quos
-        reprehenderit autem animi, nisi necessitatibus eligendi quis modi,
-        facilis ipsam nihil odit quaerat! Nisi doloribus harum culpa ipsam!
-      </p>
-      <p className="my-4">
-        Sint corporis animi repudiandae. Aliquid illum, tenetur magnam provident
-        molestiae rem doloremque aspernatur quia reiciendis est facilis enim
-        praesentium officia sequi qui debitis exercitationem quaerat hic quos
-        recusandae. Architecto repudiandae aperiam tempora iste saepe error.
-      </p>
-      <p className="my-4">
-        Provident aut suscipit aspernatur doloribus illum assumenda cupiditate
-        perferendis dolores! Obcaecati omnis magnam ipsam voluptas ipsa eos
-        explicabo quisquam architecto similique, sunt repellendus animi a
-        doloribus ab deserunt. Dicta neque nostrum modi, illum debitis dolorem.
-      </p>
     </>
   );
 };
